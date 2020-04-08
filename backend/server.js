2afa96a2afc63f53b8fa3542b6a3d79f749eaf56
terumik <t.kusaka3@gmail.com>
2020-04-08 14:44:20 -0400

    fixed animation and added resume

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 // npm run start:server (defined in package.json)
 
-const app = require("./backend/app");
+const app = require("./app");
 const debug = require("debug")("node-angular");
 const http = require("http");
 
--- src/app/portfolios/portfolio-list/portfolio-item/portfolio-item.component.ts
+++ src/app/portfolios/portfolio-list/portfolio-item/portfolio-item.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Portfolio } from 'src/app/models/portfolio.model';
 import { Router, ActivatedRoute } from '@angular/router';
+import { Animations } from 'src/app/shared/animation';
 
 @Component({
   selector: 'app-portfolio-item',
   templateUrl: './portfolio-item.component.html',
-  styleUrls: ['./portfolio-item.component.scss']
+  styleUrls: ['./portfolio-item.component.scss'],
+  animations: [Animations.slideIn, Animations.fadeIn]
 })
 export class PortfolioItemComponent implements OnInit {
 
